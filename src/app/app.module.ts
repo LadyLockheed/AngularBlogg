685ae8579b9f5be8646ccaec37f728576b1ebfeb
Karin Karlsson <karin.drake@gmail.com>
2020-03-06 12:15:27 +0100

    Skapade komponenter. Försökte ändra namn på en. Skapade routingmodul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { LatestArticleComponent } from './latest-article/latest-article.component';
+import { AllArticlesComponent } from './all-articles/all-articles.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    AddArticleComponent,
+    LatestArticleComponent,
+    AllArticlesComponent,
+    StartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
