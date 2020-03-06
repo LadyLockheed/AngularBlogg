6ff6444e499233ce056cb5a6a161e04c6fa04acf
Karin Karlsson <karin.drake@gmail.com>
2020-03-06 13:27:10 +0100

    Gjort routen till startpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component'
 
 const routes: Routes = [
-  // { path: 'heroes', component: HeroesComponent }
+  { path: 'start', component: StartPageComponent }
 ];
 
 @NgModule({
