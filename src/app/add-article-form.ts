f7299fd0f2644938d784d9fc2faadbf80998013f
Karin Karlsson <karin.drake@gmail.com>
2020-03-10 08:41:32 +0100

    solved conflict. Fixade felmeddelanden i add article. Skrivit ut lite förklaringar

--- src/app/add-article-form.ts
+++ src/app/add-article-form.ts
@@ -1,5 +1,7 @@
 export class AddArticleForm {
 
+
+	//om man vill ha optional måste man avsluta med det (alltså i värsta fall byta ordning)
     constructor(
 		public newRubrik: string,
 		public newName: string,
